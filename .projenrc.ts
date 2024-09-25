2841982ae40f56114b7066756947345725d1f90c
mtahir-cloudkitect <mtahir@cloudkitect.com>
2024-09-25 13:21:45 -0700

    feat: adding construct package

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,27 @@
 import { javascript } from "projen";
 import { monorepo } from "@aws/pdk";
+import {AwsCdkConstructLibrary} from "projen/lib/awscdk";
+
+
 const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
   name: "pnpm-pdk-sample",
   packageManager: javascript.NodePackageManager.PNPM,
   projenrcTs: true,
 });
+
+const constructs = new AwsCdkConstructLibrary({
+  defaultReleaseBranch: 'main',
+  name: `@sample/constructs`,
+  outdir: `packages/@sample/constructs`,
+  cdkVersion: '2.160.0',
+  author: 'CloudKitect Inc',
+  authorAddress: 'support@cloudkitect.com',
+  parent: project,
+  constructsVersion: '10.3.0',
+  jsiiVersion: '~5.5.0',
+  packageManager: project.package.packageManager,
+  repositoryUrl: 'https://github.com/cloudkitect/sample',
+});
+constructs.synth();
 project.synth();
---
+++ packages/@sample/constructs/API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@sample/constructs.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@sample/constructs.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@sample/constructs'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@sample/constructs.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@sample/constructs.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
